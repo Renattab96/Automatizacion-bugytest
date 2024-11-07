d792ace95ea564bd74118d7f38ed5145fb6bc1c1
Renatta Benitez <renattabenitez96@gmail.com>
2024-11-07 14:26:22 -0300

    se ajusta el caso de crear comentario

--- cypress/e2e/2.dejar-comentario.cy.js
+++ cypress/e2e/2.dejar-comentario.cy.js
@@ -1,34 +1,3 @@
-// describe('Pruebas de Comentarios', () => {
-//   beforeEach(() => {
-//     cy.viewport(1400, 900); // define la resolucion de pantalla 
-//     cy.login('renattabenitez96', 'Hol4-2024');
-//     // cy.screenshot('[PASO 1 ] COMENTARIOS-  LOGIN')
-//   });
-
-//   it('Debe permitir dejar un comentario y mostrarlo en la tabla de comentarios', () => {
-//     cy.wait(300)
-//     cy.get('.card').first().click();
-//     // cy.screenshot('[PASO 2 ] COMENTARIOS-  VISTA DEL COMENTARIO ')
-//     cy.scrollTo('bottom')
-
-//     // cy.screenshot('Comentario-Paso 3 se desplaza al final de la pagina ')
-//     cy.get(':nth-child(4) > :nth-child(5) > a').click()
-//     cy.wait(200)
-//     // cy.screenshot('[PASO 3 ] COMENTARIOS-  Detalle de Auto seleccionado  ')
-//     cy.get('#comment').scrollIntoView().should('be.visible')
-//     cy.get('.form-group').should('be.visible')
-
-//     // Ingresar un comentario y enviarlo una sola vez carga  en el siguiente login no  carga mas su comentario 
-
-//     cy.get('.col-lg-6 > .card-block > a > .img-fluid').scrollIntoView().should('be.visible')
-//     cy.get('#comment').should('be.visible').type('Este es un comentario de prueba');
-//     cy.screenshot('[PASO 4 ] COMENTARIOS-  Carga del comentario para votar   ')
-//     cy.get('.btn').click();
-
-   
-//   });
-// });
-
 
 describe('Pruebas de  Crear Comentarios', () => {
   beforeEach(() => {
@@ -41,11 +10,17 @@ describe('Pruebas de  Crear Comentarios', () => {
     cy.get('.card').first().click();
     cy.scrollTo('bottom');
 
-    cy.get('[data-testid="detalle-link"]').click();  // Selector personalizado
+    cy.get(':nth-child(5) > :nth-child(5) > a').click();  // Selector personalizado
+    cy.get('.jumbotron').should('exist')
     cy.get('#comment').scrollIntoView().should('be.visible');
     cy.get('.form-group').should('be.visible');
 
     cy.get('#comment').type('Este es un comentario de prueba');
     cy.get('.btn').click();
+
+   
+    // cy.get('.card-text').scrollIntoView().should('contain', 'Thank you for your vote!');
+    cy.get('.card-text', { timeout: 10000 }).scrollIntoView()
+     cy.get('.card-text').should('contain', 'Thank you for your vote!');
   });
 });
